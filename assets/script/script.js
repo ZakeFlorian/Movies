95202244f1022c31b999daabb62ba6185f09ea6c
Florian <stephe76bis4@outlook.fr>
2023-10-29 22:13:18 +0100

    Ajout de CSS index.html, lien entre index et profil.html.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,19 +1,39 @@
 fetch("data/movies.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        for (const movie of data.results) {
-            document.querySelector('#infos').innerHTML += `
-            <div class="col-5 mb-4">
-            <div class="card" style="width: 18rem;">
-            <img class="imgMovie card-img-top" src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="affiche film">
-  <div class="card-body">
-    <h5 class="card-title">${movie.title}</h5>
-    <p class="card-text">${movie.release_date}</p>
-    <p class="card-text">${movie.vote_average * 10}%</p>
-  </div>
-  </div>
-        `
-        }
-    })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    const infosContainer = document.querySelector('#infos');
 
+    for (const movie of data.results) {
+      // Convertissez la date de sortie au format souhaité
+      const releaseDate = new Date(movie.release_date);
+      const formattedDate = releaseDate.toLocaleString('fr-FR', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric'
+      });
+
+      // Créez un lien avec l'ID du film dans l'URL
+      const link = document.createElement('a');
+      link.href = `profil.html?id=${movie.id}`;
+      link.style.textDecoration = 'none';
+
+      // Créez la structure de la carte de film
+      const cardHTML = `
+        <div class="card mb-3 border-dark shadow-lg" style="width: 18rem;">
+          <img class="card-img-top" id="imageList" src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="affiche film">
+          <div class="card-body bg-secondary">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">${formattedDate}</p> <!-- Utilisez la date formatée -->
+            <p class="card-text">${movie.vote_average * 10}%</p>
+          </div>
+        </div>
+      `;
+
+      // Assignez la structure HTML au lien
+      link.innerHTML = cardHTML;
+
+      // Ajoutez le lien au conteneur
+      infosContainer.appendChild(link);
+    }
+  });
