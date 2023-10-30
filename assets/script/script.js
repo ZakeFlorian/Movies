8c987b683a59cddf588afaf3df28f63eed712b8f
Florian <stephe76bis4@outlook.fr>
2023-10-30 16:50:38 +0100

    bcp de changements

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,25 +1,61 @@
-fetch("data/movies.json")
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTdjYzc0OTU1MTQ5YmUyM2RmODM4MTNmMjAxYTRlOCIsInN1YiI6IjYyODM5OGJiZWM0NTUyMTAzMmE5NTcxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.REF4Oi-K06F7Jq8LolG5vPQtyeiGk3nBFdDyL1FLq7E'
+  }
+};
+fetch(`https://api.themoviedb.org/3/movie/now_playing?language=fr-FR&page=1`, options)
   .then(response => response.json())
   .then(data => {
-    console.log(data);
-    const infosContainer = document.querySelector('#infos');
-
+    console.log(data)
     for (const movie of data.results) {
-      // Convertir la date de sortie au format souhaité
-      const releaseDate = new Date(movie.release_date);
-      const formattedDate = releaseDate.toLocaleString('fr-FR', {
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric'
-      });
+      releaseHTML.innerHTML += `
+    <div class="container row">
+    <img id="imageList" src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="affiche film">
+    <h5 class="text-center">${movie.title}</h5>
+    
+    </div>
+    `
+    }
 
-      // Créer un lien avec l'ID du film dans l'URL
-      const link = document.createElement('a');
-      link.href = `profil.html?id=${movie.id}`;
-      link.style.textDecoration = 'none';
+  })
+var searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click', fetchData);
 
-      // Créer la structure de la carte de film
-      const cardHTML = `
+function fetchData() {
+  const infosContainer = document.querySelector('#infos');
+  infosContainer.innerHTML = ""
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTdjYzc0OTU1MTQ5YmUyM2RmODM4MTNmMjAxYTRlOCIsInN1YiI6IjYyODM5OGJiZWM0NTUyMTAzMmE5NTcxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.REF4Oi-K06F7Jq8LolG5vPQtyeiGk3nBFdDyL1FLq7E'
+    }
+  };
+  let movieSearch = document.querySelector('#searchInput').value
+
+  fetch(`https://api.themoviedb.org/3/search/movie?query=${movieSearch}&include_adult=false&language=fr-FR&page=1`, options)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      let index = 0
+      for (const movie of data.results) {
+        // Convertir la date de sortie au format souhaité
+        const releaseDate = new Date(movie.release_date);
+        const formattedDate = releaseDate.toLocaleString('fr-FR', {
+          day: '2-digit',
+          month: 'short',
+          year: 'numeric'
+        });
+
+        // Créer un lien avec l'ID du film dans l'URL
+        const link = document.createElement('a');
+        link.href = `profil.html?id=${movie.id}`;
+        link.style.textDecoration = 'none';
+        // Créer la structure de la carte de film
+        const cardHTML = `
         <div class="card mb-3 border-dark shadow-lg" style="width: 18rem;">
           <img class="card-img-top" id="imageList" src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="affiche film">
           <div class="card-body bg-secondary">
@@ -30,10 +66,12 @@ fetch("data/movies.json")
         </div>
       `;
 
-      // Assigner la structure HTML au lien
-      link.innerHTML = cardHTML;
+        // Assigner la structure HTML au lien
+        link.innerHTML = cardHTML;
 
-      // Ajouter le lien au conteneur
-      infosContainer.appendChild(link);
-    }
-  });
+        // Ajouter le lien au conteneur
+        infosContainer.appendChild(link);
+        index++
+      }
+    })
+}
