81f0a2c991bb9faa4bb7b094a60d3b2f2b4c7706
Florian <stephe76bis4@outlook.fr>
2023-10-30 09:48:57 +0100

    .

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,7 +5,7 @@ fetch("data/movies.json")
     const infosContainer = document.querySelector('#infos');
 
     for (const movie of data.results) {
-      // Convertissez la date de sortie au format souhaité
+      // Convertir la date de sortie au format souhaité
       const releaseDate = new Date(movie.release_date);
       const formattedDate = releaseDate.toLocaleString('fr-FR', {
         day: '2-digit',
@@ -13,27 +13,27 @@ fetch("data/movies.json")
         year: 'numeric'
       });
 
-      // Créez un lien avec l'ID du film dans l'URL
+      // Créer un lien avec l'ID du film dans l'URL
       const link = document.createElement('a');
       link.href = `profil.html?id=${movie.id}`;
       link.style.textDecoration = 'none';
 
-      // Créez la structure de la carte de film
+      // Créer la structure de la carte de film
       const cardHTML = `
         <div class="card mb-3 border-dark shadow-lg" style="width: 18rem;">
           <img class="card-img-top" id="imageList" src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="affiche film">
           <div class="card-body bg-secondary">
             <h5 class="card-title">${movie.title}</h5>
-            <p class="card-text">${formattedDate}</p> <!-- Utilisez la date formatée -->
+            <p class="card-text">${formattedDate}</p> 
             <p class="card-text">${movie.vote_average * 10}%</p>
           </div>
         </div>
       `;
 
-      // Assignez la structure HTML au lien
+      // Assigner la structure HTML au lien
       link.innerHTML = cardHTML;
 
-      // Ajoutez le lien au conteneur
+      // Ajouter le lien au conteneur
       infosContainer.appendChild(link);
     }
   });
